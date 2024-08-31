d2acc8e5a1a4f8735e08ad534dd737037b85b105
rajashree-guha <rajashree.625.guha@gmail.com>
2024-08-31 20:13:22 +0530

    Corrected error

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book("Effective Java", "Joshua Bloch");
-
         Book book2 = new Book("Clean Code", "Robert C. Martin");
 
         Library library = new Library();
@@ -9,9 +8,13 @@ public class Main {
         library.addBook(book1);
         library.addBook(book2);
 
+        System.out.println("Initial Catalog:");
         library.displayCatalog();
 
         book1.setAvailability(false);
+
+        System.out.println("\nDetails of the first book after updating availability:");
+        System.out.println(book1.getBookDetails());
 
         System.out.println("\nUpdated Catalog:");
         library.displayCatalog();
