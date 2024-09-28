b02de7dda106d2ab29d09d319318772e511769b6
rajashree-guha <rajashree.625.guha@gmail.com>
2024-09-28 14:30:15 +0530

    Milestone 2

--- src/Book.java
+++ src/Book.java
@@ -3,17 +3,41 @@ public class Book {
     private String author;
     private boolean isAvailable;
 
+    private static int totalBooks = 0;
+    private static int availableBooks = 0;
+
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
         this.isAvailable = true;
+        totalBooks++;
+        availableBooks++;
     }
 
     public String getBookDetails() {
         return "Title: " + title + ", Author: " + author + ", Available: " + isAvailable;
     }
 
-    public void setAvailability(boolean availability) {
-        this.isAvailable = availability;
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            availableBooks--;
+        } else {
+            System.out.println("Book is already borrowed.");
+        }
+    }
+
+    public void returnBook() {
+        if (!isAvailable) {
+            isAvailable = true;
+            availableBooks++;
+        } else {
+            System.out.println("Book is already available.");
+        }
+    }
+
+    public static void displayBookStats() {
+        System.out.println("Total Books in Library: " + totalBooks);
+        System.out.println("Available Books: " + availableBooks);
     }
 }
