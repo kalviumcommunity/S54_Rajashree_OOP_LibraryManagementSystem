dd8bbb15aae125fceb7b091123448e0c3045827d
rajashree-guha <rajashree.625.guha@gmail.com>
2024-11-18 16:34:43 +0530

    Implemented Polymorphism

--- src/Library.java
+++ src/Library.java
@@ -5,7 +5,6 @@ public class Library {
 
     public Library() {
         this.catalog = new ArrayList<>();
-
     }
 
     public void addBook(Book book) {
@@ -21,5 +20,35 @@ public class Library {
 
     public ArrayList<Book> getCatalog() {
         return new ArrayList<>(catalog);
+    }
+
+    // Search by Title
+    public void searchBooks(String title) {
+        System.out.println("Searching for books by Title: " + title);
+        for (Book book : catalog) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                System.out.println(book.getBookDetails());
+            }
+        }
+    }
+
+    // Search by Author
+    public void searchBooksByAuthor(String author) {
+        System.out.println("Searching for books by Author: " + author);
+        for (Book book : catalog) {
+            if (book.getAuthor().equalsIgnoreCase(author)) {
+                System.out.println(book.getBookDetails());
+            }
+        }
+    }
+
+    // Search by Availability
+    public void searchBooks(boolean isAvailable) {
+        System.out.println("Searching for books by Availability: " + (isAvailable ? "Available" : "Borrowed"));
+        for (Book book : catalog) {
+            if (book.isAvailable() == isAvailable) {
+                System.out.println(book.getBookDetails());
+            }
+        }
     }
 }
