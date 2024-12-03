ba2d02f51794b927f721e6a803ccdd1e8382789d
rajashree-guha <rajashree.625.guha@gmail.com>
2024-12-03 20:22:29 +0530

    Implemented Liskov Substitution Principle

--- src/SpecialCollectionBook.java
+++ src/SpecialCollectionBook.java
@@ -1,4 +1,4 @@
-public class SpecialCollectionBook extends DigitalBook {
+public class SpecialCollectionBook extends DigitalBook implements BorrowableItem {
     private String accessLevel;
 
     public SpecialCollectionBook(String title, String author, String fileFormat, double fileSize, String accessLevel) {
@@ -17,5 +17,25 @@ public class SpecialCollectionBook extends DigitalBook {
     @Override
     public String getItemDetails() {
         return super.getItemDetails() + ", Access Level: " + accessLevel;
+    }
+
+    @Override
+    public void borrowItem() {
+        if (isAvailable()) {
+            setAvailable(false);
+            System.out.println("Special Collection book borrowed successfully!");
+        } else {
+            System.out.println("Book is not available for borrowing.");
+        }
+    }
+
+    @Override
+    public void returnItem() {
+        if (!isAvailable()) {
+            setAvailable(true);
+            System.out.println("Special Collection book returned successfully!");
+        } else {
+            System.out.println("Book is already available.");
+        }
     }
 }
