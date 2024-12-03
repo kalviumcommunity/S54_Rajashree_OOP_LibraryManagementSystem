3b7c8596d0ad5bcf96d25c9e01e91be93b886507
rajashree-guha <rajashree.625.guha@gmail.com>
2024-12-03 16:09:34 +0530

    Implemented Liskov Substitution Principle

--- src/DigitalBook.java
+++ src/DigitalBook.java
@@ -28,4 +28,16 @@ public class DigitalBook extends Book {
     public String getItemDetails() {
         return super.getItemDetails() + ", File Format: " + fileFormat + ", File Size: " + fileSize + "MB";
     }
+
+    // Overriding borrowItem to reflect the behavior for digital books
+    @Override
+    public void borrowItem() {
+        System.out.println("Digital books cannot be borrowed. Please download or access the file.");
+    }
+
+    // Overriding returnItem to reflect the behavior for digital books
+    @Override
+    public void returnItem() {
+        System.out.println("Digital books do not need to be returned.");
+    }
 }
